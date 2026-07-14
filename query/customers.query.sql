2b16b221c63c1a24aa37919041f15e329ad4b82a
Raushan Kumar <sraushan890@gmail.com>
2026-07-14 08:46:16 +0530

    Add SQL queries for customer data analysis
    - Implemented various queries to filter and count female customers from Oregon and New York.
    - Added examples of using NOT operator for filtering out specific genders and ages.
    - Included comparison operators and logical operators in the context of customer income and age.
    - Provided queries to calculate average income for customers aged between 20 and 50.

---
+++ query/customers.query.sql
@@ -0,0 +1,76 @@
+-- filte the data from employees tables
+--  SQL filter strcuture according how we write the sql filter query depending upon your query return data
+
+-- select first_name, last_name  from "employees" where (first_name ='Georgi' and last_name ='Facello' and hire_date ='1986-06-26')or (first_name = 'Bezalel' and last_name ='Simmel') ;
+
+-- Q1. How many female customers Do we have from the state of oregon (OR) and new york (NY)
+
+
+select firstname, lastname,state,gender from "customers" where (state='OR' or state='NY') and (gender='F');
+select count(firstname) from "customers" where (state='OR' or state='NY') and (gender='F');
+
+-- WHAT IF
+-- You want to filter on everything but ....? 
+-- we can use another filter keyword called (! || NOT)
+
+select firstname, lastname, gender from "customers" where not gender ='M';
+
+-- Q HOW MANY CUSTOMERS AREN"T 55?
+
+select  firstname, lastname, gender, age from "customers" where not age =55;
+
+select  firstname, lastname, gender, age,count(age) from "customers" where  age =55;
+
+-- remove the data from specific tables 
+select  count(age) from "customers" where not  age =55;
+
+-- comparison operators
+
+-- who over the age of 44 has an income of 100000
+/*
+ * 10 > 20 false
+ * 10 < 20 true
+ * 10 <= 20  true
+ * 10 >= 9 true
+ * 0 = 0 true 
+ * 1 != 0 true 
+ * 
+ */
+
+-- How many female customers do we have from the state of Oregon (OR)
+
+select  count(state)  from "customers"
+where ( gender ='M') and (state= 'OR') ;
+
+-- Who over the age of 44 has an income of 100 000 or more?
+select firstname , lastname, age, gender, income from "customers"
+where (age > 44) and (income = 100000);
+
+-- Who between the ages of 30 and 50 has an income of less than 50 000?
+
+select firstname , lastname, age, gender, income from "customers"
+where (age >= 30 and age <= 50) and (income < 50000);
+
+select firstname , lastname, age, gender, income from "customers"
+where (age between 30 and  50) and (income < 50000);
+
+-- What is the average income between the ages of 20 and 50?
+
+select AVG(income) from "customers" where age between  20 and 50;
+
+
+-- Logical Operators  (AND OR NOT)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
